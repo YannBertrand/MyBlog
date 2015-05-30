ef187697bd0a9655b34f3c2bc2423e29799fe3c4
Yann Bertrand <yann@bertrand.com>
2015-05-30 11:54:10 +0200

    feat(articlehistory): create model and controller

---
+++ api/controllers/ArticleHistoryController.js
@@ -0,0 +1,11 @@
+/**
+ * ArticleHistoryController
+ *
+ * @description :: Server-side logic for managing articlehistories
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/ArticleHistory.js
@@ -0,0 +1,67 @@
+/**
+* ArticleHistory.js
+*
+* @description :: Article History
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  schema: true,
+  attributes: {
+
+    // A One-to-Many association with the Article model
+    article: {
+      model: 'Article',
+      required: true
+    },
+
+    revisionNumber: {
+      type: 'integer',
+      required: true
+    },
+
+    title: {
+      type: 'string',
+      required: true,
+      unique: true,
+      minLength: 3
+    },
+
+    // The slug is an unique identifier to an Article
+    slug: {
+      type: 'string',
+      required: true,
+      unique: true,
+      alphanumericdashed: true,
+      minLength: 2
+    },
+
+    content: {
+      type: 'text',
+      required: true,
+      minLength: 15
+    },
+
+    // Is this article supposed to be shown?
+    shown: {
+      type: 'boolean',
+      required: true,
+      defaultsTo: true
+    },
+
+    // A Many-to-Many association with the Tag model
+    tags: {
+      collection: 'Tag',
+      via: 'articleVersions'
+    },
+
+    // A One-to-Many association with the Comment model
+    comments: {
+      collection: 'Comment',
+      via: 'articleVersion'
+    }
+
+  }
+};
+
